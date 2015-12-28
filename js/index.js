d24bf77a054317b2ff076b3c0d6b9548a9e655cd
Carlos Liu <carlos@CarlosdeMacBook-Pro.local>
2015-12-28 18:48:13 +0800

    Ver.1

---
+++ js/index.js
@@ -0,0 +1,40 @@
+ function loaderSpinner() {
+    $(window).load(function() {
+        var loader = $('.loader');
+var wHeight = $(window).height();
+var wWidth = $(window).width();
+var i = 0;
+/*Center loader on half screen */
+loader.css({
+    top: wHeight / 2 - 2.5,
+    left: wWidth / 2 - 200
+ })
+      
+  do{
+    loader.animate({
+      width: i
+    },10)
+    i+=3;
+  } while(i <= 400)
+  if(i === 402){
+    loader.animate({
+      left: 0,
+      width: '100%'
+    })
+    loader.animate({
+      top: '0',
+      height: '100vh'
+    })
+  }
+      
+      /* This line hide loader and show content */
+      setTimeout(function(){
+        $('.content').fadeIn("slow");
+        (loader).fadeOut("fast");
+        /*Set time in milisec */
+      },3500);
+    });
+
+}
+
+loaderSpinner();
